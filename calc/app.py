1bb8bfd75d42ef8ccd7d0d50d66a8acc9db379e8
Natalie Chapman <natalie.c.chapman628@gmail.com>
2023-11-19 22:05:50 -0800

    added other operations

--- calc/app.py
+++ calc/app.py
@@ -9,3 +9,21 @@ def render_add_page():
     a = int(request.args["a"])
     b = int(request.args["b"])
     return str(add(a, b))
+
+@app.route("/sub")
+def render_sub_page():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(sub(a, b))
+
+@app.route("/mult")
+def render_mult_page():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(mult(a, b))
+
+@app.route("/div")
+def render_div_page():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(div(a, b))
