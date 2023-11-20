895a45908ee17ce2e17ed5d94946076812b7ac60
Natalie Chapman <natalie.c.chapman628@gmail.com>
2023-11-19 22:03:21 -0800

    added add function

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,11 @@
 # Put your app in here.
+from flask import Flask, request
+from operations import *
+
+app = Flask(__name__)
+
+@app.route("/add")
+def render_add_page():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(add(a, b))
